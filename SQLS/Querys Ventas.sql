87779650b7a7f785fc1e8607360ada0af4eec849
andresarl16 <andrucho01@gmail.com>
2021-07-06 22:01:50 -0400

    fff

--- SQLS/Querys Ventas.sql
+++ SQLS/Querys Ventas.sql
@@ -1,1 +1,19 @@
-SELECT * FROM "Hechos_Ventas"
+--- Unidades vendidas de cada producto ---
+SELECT id_dtienda, nombre, SUM(unidades) AS unidades, anio, mes, semana, dia FROM "Hechos_Ventas"
+INNER JOIN "Dimension_Producto" USING (id_dproducto)
+INNER JOIN "Dimension_Tiempo_Ventas" USING (id_dtiempo)
+GROUP BY id_dtienda, nombre, anio, mes, semana, dia
+ORDER BY nombre ASC;
+
+--- Monto total de ventas por producto ---
+SELECT id_dtienda, nombre, SUM(unidades * precio) AS monto, anio, mes, semana, dia FROM "Hechos_Ventas"
+INNER JOIN "Dimension_Producto" USING (id_dproducto)
+INNER JOIN "Dimension_Tiempo_Ventas" USING (id_dtiempo)
+GROUP BY id_dtienda, nombre, anio, mes, semana, dia
+ORDER BY nombre ASC;
+
+--- Monto total de ventas ---
+SELECT id_dtienda, SUM(unidades * precio) AS monto, anio, mes, semana, dia FROM "Hechos_Ventas"
+INNER JOIN "Dimension_Producto" USING (id_dproducto)
+INNER JOIN "Dimension_Tiempo_Ventas" USING (id_dtiempo)
+GROUP BY id_dtienda, anio, mes, semana, dia;
