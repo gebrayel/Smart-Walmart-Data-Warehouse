381ee6897143dc3e8db5e7bfcb739d3b83553b6e
andresarl16 <andrucho01@gmail.com>
2021-07-07 00:10:17 -0400

    fdkfsd

--- SQLS/Querys Inventario.sql
+++ SQLS/Querys Inventario.sql
@@ -1,17 +1,17 @@
 --- Producto mas Rotado ---
-SELECT DISTINCT ON (id_dtienda) id_dtienda, nombre, SUM(unidades) AS unidades, anio, mes, semana, dia FROM "Hechos_Inventarios"
+SELECT DISTINCT ON (dia) id_dtienda, nombre, SUM(unidades) AS unidades, dia FROM "Hechos_Inventarios"
 INNER JOIN "Dimension_Producto" USING (id_dproducto)
 INNER JOIN "Dimension_Tiempo_Inventario" USING (id_dtiempo)
-GROUP BY id_dtienda, nombre, anio, mes, semana, dia
-ORDER BY id_dtienda, anio, mes, semana, dia, unidades DESC;
+GROUP BY id_dtienda, nombre, dia
+ORDER BY dia, id_dtienda, unidades DESC;
 
 --- Estanteria mas Rotado ---
-SELECT DISTINCT ON (id_dtienda) id_dtienda, id_destanteria, nombre, SUM(unidades) AS unidades, anio, mes, semana, dia FROM "Hechos_Inventarios"
+SELECT DISTINCT ON (dia) id_dtienda, id_destanteria, nombre, SUM(unidades) AS unidades, dia FROM "Hechos_Inventarios"
 INNER JOIN "Dimension_Producto" USING (id_dproducto)
 INNER JOIN "Dimension_Tiempo_Inventario" USING (id_dtiempo)
 INNER JOIN "Dimension_Estanteria" USING (id_destanteria)
 GROUP BY id_dtienda, id_destanteria, nombre, anio, mes, semana, dia
-ORDER BY id_dtienda, anio, mes, semana, dia, unidades DESC;
+ORDER BY dia, id_dtienda, unidades DESC;
 
 --- Categoria mas Rotada ---
 SELECT DISTINCT ON (id_dtienda) id_dtienda, "Dimension_Categoria".nombre, SUM(unidades) AS unidades FROM "Hechos_Inventarios"
